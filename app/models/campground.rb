0cdce95251d1d2cc1c9d38023546506c1b9eaf85
Christian Ullman <christian.ullman@endurance.com>
2021-07-09 06:33:42 -0500

    adding attrs: campsites, booked_dates, price_range

--- app/models/campground.rb
+++ app/models/campground.rb
@@ -1,17 +1,25 @@
+require 'date'
+
 class Campground < ApplicationRecord
   attr_reader :campsites, :booked_dates, :price_range
 
-  has_many :campsites
+  has_many :campsite
 
   def campsites
-    @campsites = :placeholder
+    @campsites = self.campsites.collect { |site| site.name }
   end
 
   def booked_dates
-    @booked_dates = :placeholder
+    @booked_dates = self.campsites.collect do |site|
+      bookings = []
+      self.campsites.booked_dates.split(",").each do |booking|
+        bookings << DateTime.parse(booking).to_date
+      end
+      bookings
+    end
   end
 
   def price_range
-    @price_range = :placeholder
+    @price_range = self.campsites.collect { |site| site.price }.minmax
   end
 end
